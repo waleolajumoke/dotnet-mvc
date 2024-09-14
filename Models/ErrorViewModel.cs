554dd68284eea8e1a636394936495dc092eb6474
waleolajumoke <waleolajumoke@gmail.com>
2024-09-14 12:21:06 +0100

    sending the dotnet app to github

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetproject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
